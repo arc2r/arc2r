2a9a8adceec76e76e82bd402ff9f4c0881b83e42
Nils Ratnaweera <rata@zhaw.ch>
2021-01-26 07:04:32 +0100

    added "gemeindegrenzen_zh" dataset

---
+++ data-raw/gemeindegrenzen_zh.R
@@ -0,0 +1,7 @@
+## code to prepare `DATASET` dataset goes here
+
+library(sf)
+
+gemeindegrenzen_zh <- sf::st_read("data-raw/Gemeindegrenzen_ZH/Gemeindegrenzen.gpkg","UP_GEMEINDEN_F")
+
+usethis::use_data(gemeindegrenzen_zh, overwrite = TRUE)
