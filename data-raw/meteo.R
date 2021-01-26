8cf95e26831e6458dcdd2353903232f48e5510bf
Nils Ratnaweera <rata@zhaw.ch>
2021-01-26 08:15:44 +0100

    some changes

--- data-raw/meteo.R
+++ data-raw/meteo.R
@@ -2,5 +2,5 @@
 meteo <- readr::read_table("data-raw/meteo/order_52252_data.txt")
 meteo_legend <- readr::read_delim("data-raw/meteo/order_52252_legend.csv", ";")
 
-usethis::use_data(meteo)
-usethis::use_data(meteo_legend)
+usethis::use_data(meteo,overwrite = TRUE)
+usethis::use_data(meteo_legend, overwrite = TRUE)
