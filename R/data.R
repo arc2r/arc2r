552a9fcc14122e6dbc71699c120325674340e859
Nils Ratnaweera <rata@zhaw.ch>
2021-01-26 07:15:12 +0100

    add description

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Gemeindegrenzen im Kanton Zürich
+#'
+#' This dataset contains the boarder for the municipalities in the canton of Zurich
+#' The data was obtained from maps.zh.ch
+#'
+#' @format A sf object with 167 features and 10 variables
+#' \describe{
+#'   \item{BFS}{BFS Number}
+#'   \item{ARPS}{Add description here}
+#'   \item{BEZIRKSNAME}{Add description here}
+#'   \item{ART_TEXT}{Add description here}
+#'   \item{GEMEINDENAME}{Add description here}
+#'   \item{AREA_ROUND}{Add description here}
+#'   \item{SHAPE_AREA}{Add description here}
+#'   \item{SHAPE_LEN}{Add description here}
+#'   \item{geom}{Add description here}
+#' }
+#' @source \url{http://maps.zh.ch/?topic=ortho19ZH&showtab=ogddownload}
+"gemeindegrenzen_zh"
