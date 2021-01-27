06ee369af2663847af898c5cf76ddbf80144dbac
Bakogiannis Nikos <bako@zhaw.ch>
2021-01-27 09:00:04 +0100

    2nd try with raster

--- data-raw/population_india.R
+++ data-raw/population_india.R
@@ -4,3 +4,4 @@ pop_india2010 <- raster::raster("data-raw/population_india/india_2010.tif")
 
 usethis::use_data(pop_india2000, overwrite = TRUE)
 usethis::use_data(pop_india2010, overwrite = TRUE)
+
