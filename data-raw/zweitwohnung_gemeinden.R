f422fa85faf114c15e5c223763ad55f5521d0ca8
Bakogiannis Nikos <bako@zhaw.ch>
2021-01-27 16:11:00 +0100

    fix error v3

--- data-raw/zweitwohnung_gemeinden.R
+++ data-raw/zweitwohnung_gemeinden.R
@@ -1,4 +1,4 @@
 
-zweitwohnung_gemeinden <- sf::st_read("data-raw/zweitwohnung_gemeinden/zweitwohnung_gemeinden.gpkg")
+zweitwohnung_gemeinden <- sf::st_read("data-raw/zweitwohnung_gemeinden/zweitwohnung_gemeinden.gpkg","zweitwohnung_gemeinden")
 
 usethis::use_data(zweitwohnung_gemeinden, overwrite = TRUE)
