143dfdfc7e9fb4816b3342c585b35696d9eebda6
Nils Ratnaweera <rata@zhaw.ch>
2021-02-03 09:12:51 +0100

    see #10

--- data-raw/swissAlti3D.R
+++ data-raw/swissAlti3D.R
@@ -1,4 +1,7 @@
 
 swissAlti3D <- raster::raster("data-raw/swissAlti3D/10m/SWISSALTI3D_10_TIFF_CHLV95_LN02_2600_1196.tif")
 
+# force raster into memory: https://stackoverflow.com/a/54314986/4139249
+swissAlti3D <- readAll(swissAlti3D)
+
 usethis::use_data(swissAlti3D, overwrite = TRUE)
