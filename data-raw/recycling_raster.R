6919edbc3c4e76316abebd4081cef03aa62a4416
Nils Ratnaweera <rata@zhaw.ch>
2021-02-04 15:29:11 +0100

    #10

--- data-raw/recycling_raster.R
+++ data-raw/recycling_raster.R
@@ -1,4 +1,6 @@
 
 recycling_raster <- raster::raster("data-raw/recycling_raster/abfall_raster.tif")
 
+recycling_raster <- raster::readAll(recycling_raster)
+
 usethis::use_data(recycling_raster, overwrite = TRUE)
