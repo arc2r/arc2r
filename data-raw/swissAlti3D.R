dc9d372c13354cb274d5fbf5332cb11b018194c2
Nils Ratnaweera <rata@zhaw.ch>
2021-02-04 15:29:22 +0100

    add packge

--- data-raw/swissAlti3D.R
+++ data-raw/swissAlti3D.R
@@ -2,6 +2,6 @@
 swissAlti3D <- raster::raster("data-raw/swissAlti3D/10m/SWISSALTI3D_10_TIFF_CHLV95_LN02_2600_1196.tif")
 
 # force raster into memory: https://stackoverflow.com/a/54314986/4139249
-swissAlti3D <- readAll(swissAlti3D)
+swissAlti3D <- raster::readAll(swissAlti3D)
 
 usethis::use_data(swissAlti3D, overwrite = TRUE)
