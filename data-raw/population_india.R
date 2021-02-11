f237bd094de9ce2ee9c7ee8f5ab7d890d6a068cb
Nils Ratnaweera <rata@zhaw.ch>
2021-02-11 14:24:45 +0100

    #10

--- data-raw/population_india.R
+++ data-raw/population_india.R
@@ -4,7 +4,8 @@ pop_india2010 <- raster::raster("data-raw/population_india/india_2010.tif")
 
 # //todo add readAll
 
-
+pop_india2000 <- raster::readAll(pop_india2000)
+pop_india2010 <- raster::readAll(pop_india2010)
 
 usethis::use_data(pop_india2000, overwrite = TRUE)
 usethis::use_data(pop_india2010, overwrite = TRUE)
