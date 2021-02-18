caab9cf5f8cea098798415f70f2012bb5d3680a9
Nils Ratnaweera <rata@zhaw.ch>
2021-02-18 09:27:21 +0100

    simplify this dataset

--- data-raw/haltestelle_bahn.R
+++ data-raw/haltestelle_bahn.R
@@ -1,4 +1,8 @@
 
 haltestelle_bahn <- sf::st_read("data-raw/archive_todelete/other/Haltestelle_Bahn_CH.shp")
 
+haltestelle_bahn <- haltestelle_bahn[,"name"]
+
+haltestelle_bahn <- sf::st_zm(haltestelle_bahn)
+
 usethis::use_data(haltestelle_bahn, overwrite = TRUE)
