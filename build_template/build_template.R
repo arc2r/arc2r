b4f76c2c01633b84d957964bac92e03bfb01f61b
Nils Ratnaweera <rata@zhaw.ch>
2021-02-23 09:09:09 +0100

    adding a script to build templates in a github action

---
+++ build_template/build_template.R
@@ -0,0 +1,34 @@
+
+library(purrr)
+
+library(knitr)
+library(stringr)
+
+zipfile_path <- "build_template/book.zip"
+templatepath <- file.path("inst","rstudio", "templates", "project", "follow_along_resources")
+
+download.file("https://github.com/arc2r/templatefiles/archive/master.zip",zipfile_path)
+
+
+untar(zipfile_path,exdir = templatepath,extras = "--strip-components=1")
+
+mydirs <- list.dirs(templatepath, full.names = FALSE, recursive = FALSE)
+
+
+dcf_content <- c(
+  "Title: arc2r: All things R Spatial",
+  "Binding: follow_along_skeleton",
+  "OpenFiles: Readme.Rmd",
+  "",
+  "Parameter: chapter",
+  "Widget: SelectInput",
+  "Label: Which chapter do you want to work on?",
+  paste("Fields:",paste(mydirs,collapse = ", ")),
+  paste("Default:", mydirs[1]),
+  "Position: left"
+)
+
+writeLines(dcf_content, "inst/rstudio/templates/project/follow_along.dcf")
+
+
+
